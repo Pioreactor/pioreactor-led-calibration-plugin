03ed7ce88cdf2ba3822a02242dae1ff07d34f617
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2025-01-19 20:57:15 -0500

    align it with the existing light/dark cycle automation

--- led_calibration_plugin/calibrated_light_dark_cycle.py
+++ led_calibration_plugin/calibrated_light_dark_cycle.py
@@ -1,48 +1,48 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-from msgspec.json import decode
+from typing import Optional
+
 from pioreactor.automations import events
 from pioreactor.automations.led.base import LEDAutomationJobContrib
+from pioreactor.calibrations import load_active_calibration
 from pioreactor.exc import CalibrationError
 from pioreactor.types import LedChannel
 from pioreactor.utils import clamp
 from pioreactor.utils import local_persistent_storage
 
-from .led_calibration import LEDCalibration
-
 
 class CalibratedLightDarkCycle(LEDAutomationJobContrib):
     """
-    Follows as h light / h dark cycle. Starts light ON.
+    Follows as min light / min dark cycle. Starts light ON.
     """
 
-    automation_name = "calibrated_light_dark_cycle"
+    automation_name: str = "calibrated_light_dark_cycle"
     published_settings = {
         "duration": {
             "datatype": "float",
             "settable": False,
             "unit": "min",
         },
-        "light_intensity": {"datatype": "float", "settable": True, "unit": "AU"},
-        "light_duration_hours": {"datatype": "integer", "settable": True, "unit": "h"},
-        "dark_duration_hours": {"datatype": "integer", "settable": True, "unit": "h"},
+        "light_intensity": {"datatype": "float", "settable": True, "unit": "%"},
+        "light_duration_minutes": {"datatype": "integer", "settable": True, "unit": "min"},
+        "dark_duration_minutes": {"datatype": "integer", "settable": True, "unit": "min"},
     }
 
     def __init__(
         self,
-        light_intensity: float,
-        light_duration_hours: int,
-        dark_duration_hours: int,
+        light_intensity: float | str,
+        light_duration_minutes: int | str,
+        dark_duration_minutes: int | str,
         **kwargs,
     ):
         super().__init__(**kwargs)
-        self.hours_online: int = -1
+        self.minutes_online: int = -1
         self.light_active: bool = False
-        self.channels: list[LedChannel] = ["C", "D"]
+        self.channels: list[LedChannel] = ["D", "C"]
         self.set_light_intensity(light_intensity)
-        self.light_duration_hours = float(light_duration_hours)
-        self.dark_duration_hours = float(dark_duration_hours)
+        self.light_duration_minutes = float(light_duration_minutes)
+        self.dark_duration_minutes = float(dark_duration_minutes)
 
         with local_persistent_storage("active_calibrations") as cache:
             for channel in self.channels:
@@ -52,55 +52,66 @@ class CalibratedLightDarkCycle(LEDAutomationJobContrib):
     def channel_to_led_device(self, channel: LedChannel) -> str:
         return f"led_{channel}"
 
-    def execute(self) -> events.AutomationEvent:
-        self.hours_online += 1
-        return self.trigger_leds(self.hours_online)
+    def execute(self) -> Optional[events.AutomationEvent]:
+        # runs every minute
+        self.minutes_online += 1
+        return self.trigger_leds(self.minutes_online)
 
-    def calculate_intensity_percent(self, channel):
-        with local_persistent_storage("current_led_calibration") as cache:
-            led_calibration = decode(
-                cache[self.channel_to_led_device(channel)], type=LEDCalibration
-            )
+    def calculate_intensity_percent(self, channel: str) -> float:
 
-            intensity_percent = (
-                self.light_intensity - led_calibration.curve_data_[1]
-            ) / led_calibration.curve_data_[0]
+        led_calibration = load_active_calibration(self.channel_to_led_device(channel))
 
-            return clamp(0, intensity_percent, 100)
+        intensity_percent = led_calibration.ipredict(self.light_intensity)
 
-    def trigger_leds(self, hours: int) -> events.AutomationEvent:
+        return clamp(0, intensity_percent, 100)
 
-        cycle_duration = self.light_duration_hours + self.dark_duration_hours
+    def trigger_leds(self, minutes: int) -> Optional[events.AutomationEvent]:
+        """
+        Changes the LED state based on the current minute in the cycle.
 
-        if ((hours % cycle_duration) < self.light_duration_hours) and (not self.light_active):
+        The light and dark periods are calculated as multiples of 60 minutes, forming a cycle.
+        Based on where in this cycle the current minute falls, the light is either turned ON or OFF.
+
+        Args:
+            minutes: The current minute of the cycle.
+
+        Returns:
+            An instance of AutomationEvent, indicating that LEDs' status might have changed.
+            Returns None if the LEDs' state didn't change.
+        """
+        cycle_duration_min = int(self.light_duration_minutes + self.dark_duration_minutes)
+        if ((minutes % cycle_duration_min) < (self.light_duration_minutes)) and (
+            not self.light_active
+        ):
             self.light_active = True
 
             for channel in self.channels:
                 intensity_percent = self.calculate_intensity_percent(channel)
                 self.set_led_intensity(channel, intensity_percent)
 
-            return events.ChangedLedIntensity(f"{hours}h: turned on LEDs.")
+            return events.ChangedLedIntensity(f"{minutes:.1f}min: turned on LEDs.")
 
-        elif ((hours % cycle_duration) >= self.light_duration_hours) and (self.light_active):
+        elif ((minutes % cycle_duration_min) >= (self.light_duration_minutes)) and (
+            self.light_active
+        ):
             self.light_active = False
             for channel in self.channels:
                 self.set_led_intensity(channel, 0)
-            return events.ChangedLedIntensity(f"{hours}h: turned off LEDs.")
+            return events.ChangedLedIntensity(f"{minutes:.1f}min: turned off LEDs.")
 
         else:
-            return events.NoEvent(f"{hours}h: no change.")
+            return None
 
-    def set_dark_duration_hours(self, hours: int):
+    # minutes setters
+    def set_dark_duration_minutes(self, minutes: int):
+        self.dark_duration_minutes = minutes
 
-        self.dark_duration_hours = hours
+        self.trigger_leds(self.minutes_online)
 
-        self.trigger_leds(self.hours_online)
+    def set_light_duration_minutes(self, minutes: int):
+        self.light_duration_minutes = minutes
 
-    def set_light_duration_hours(self, hours: int):
-
-        self.light_duration_hours = hours
-
-        self.trigger_leds(self.hours_online)
+        self.trigger_leds(self.minutes_online)
 
     def set_light_intensity(self, intensity_au):
         # this is the setter of light_intensity attribute, eg. called when updated over MQTT
@@ -116,6 +127,6 @@ class CalibratedLightDarkCycle(LEDAutomationJobContrib):
             pass
 
     def set_duration(self, duration: float) -> None:
-        if duration != 60:
-            self.logger.warning("Duration should be set to 60.")
+        if duration != 1:
+            self.logger.warning("Duration should be set to 1.")
         super().set_duration(duration)
--- led_calibration_plugin/test_led_calibration.py
+++ led_calibration_plugin/test_led_calibration.py
@@ -7,7 +7,7 @@ import pytest
 from pioreactor.exc import CalibrationError
 from pioreactor.utils import local_intermittent_storage
 from pioreactor.utils import local_persistent_storage
-from pioreactor.utils.timing import current_utc_timestamp
+from pioreactor.utils.timing import current_utc_datetime
 from pioreactor.whoami import get_unit_name
 
 from .calibrated_light_dark_cycle import CalibratedLightDarkCycle
@@ -24,22 +24,22 @@ def test_led_fails_if_calibration_not_present():
 
     with local_persistent_storage("active_calibrations") as cache:
         if "led_C" in cache:
-            del cache["led_C"]
+            cache.pop("led_C")
         if "led_D" in cache:
-            del cache["led_D"]
+            cache.pop("led_D")
 
     with pytest.raises(CalibrationError):
 
         with CalibratedLightDarkCycle(
-            duration=0.01,
+            duration=1,
             light_intensity=-1,
-            light_duration_hours=16,
-            dark_duration_hours=8,
+            light_duration_minutes=16,
+            dark_duration_minutes=8,
             unit=unit,
             experiment=experiment,
         ):
 
-            pause(8)
+            pass
 
 
 def test_set_intensity_au_above_max() -> None:
@@ -47,7 +47,7 @@ def test_set_intensity_au_above_max() -> None:
     unit = get_unit_name()
 
     cal = LEDCalibration(
-        created_at=current_utc_timestamp(),
+        created_at=current_utc_datetime(),
         calibrated_on_pioreactor_unit=unit,
         calibration_name=experiment,
         curve_data_=[1, 0],
@@ -60,14 +60,14 @@ def test_set_intensity_au_above_max() -> None:
     cal.set_as_active_calibration_for_device("led_D")
 
     with CalibratedLightDarkCycle(
-        duration=0.01,
+        duration=1,
         light_intensity=1500,
-        light_duration_hours=16,
-        dark_duration_hours=8,
+        light_duration_minutes=16,
+        dark_duration_minutes=8,
         unit=unit,
         experiment=experiment,
     ) as lc:
-
+        pause(10)
         assert lc.light_intensity == 1500  # test returns light_intensity (au)
 
         lc.set_light_intensity(2000)
@@ -80,7 +80,7 @@ def test_set_intensity_au_negative() -> None:
     unit = get_unit_name()
 
     cal = LEDCalibration(
-        created_at=current_utc_timestamp(),
+        created_at=current_utc_datetime(),
         calibrated_on_pioreactor_unit=unit,
         calibration_name=experiment,
         curve_data_=[1, 0],
@@ -93,10 +93,10 @@ def test_set_intensity_au_negative() -> None:
     cal.set_as_active_calibration_for_device("led_D")
 
     with CalibratedLightDarkCycle(
-        duration=0.01,
+        duration=1,
         light_intensity=-1,
-        light_duration_hours=16,
-        dark_duration_hours=8,
+        light_duration_minutes=16,
+        dark_duration_minutes=8,
         unit=unit,
         experiment=experiment,
     ) as lc:
